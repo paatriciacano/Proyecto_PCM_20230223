bca30ba1e63627ec749da9e4dd73ebbcbcc45281
paatriciacano <124888427+paatriciacano@users.noreply.github.com>
2023-02-23 21:46:55 +0100

    Initial commit

---
+++ src/prueba/Colegio.java
@@ -0,0 +1,29 @@
+package prueba;
+
+public class Colegio {
+
+	private String nombre;
+	private int numeroAlumnos;
+
+	public Colegio(String nombre, int numeroAlumnos) {
+		this.nombre = nombre;
+		this.numeroAlumnos = numeroAlumnos;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumeroAlumnos() {
+		return numeroAlumnos;
+	}
+
+	public void setNumeroAlumnos(int numeroAlumnos) {
+		this.numeroAlumnos = numeroAlumnos;
+	}
+
+}
